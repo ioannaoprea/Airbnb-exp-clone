fe15e954e98e23529af33dbb122329e51a3c5f42
Ioana Oprea <ioana176@yahoo.com>
2023-09-18 16:18:14 +0300

    1

---
+++ src/components/Card.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+export default function Card(props) {
+  
+  let badgeText
+  if (props.item.openSpots === 0) {
+    badgeText="Sold Out"
+  } else if (props.item.location === "Online"){
+    badgeText="Online"
+  }
+
+  return (
+    <div className="card">
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+        <img src={`../images/${props.item.coverImg}`} className="card--img"/>
+        <div className="card--stats">
+            <img src="./images/star.png" className="card--star"/>
+            <span>{props.item.stats.rating} </span>
+            <span className="gray"> ({props.item.stats.reviewCount}) · </span>
+            <span className="gray"> {props.item.location}</span>
+        </div>
+        <p className="card--title">{props.item.title}</p>
+        <p className="card--price"><span className="bold">From ${props.item.price}</span> / person</p>
+    </div>
+  )
+}
---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Hero() {
+    return (
+        <section className="hero">
+            <img className="hero--img" src="./images/photo-grid.png" alt="multiple photos" />
+            <h1 className="hero--title">Online Experiences</h1>
+            <p className="hero--text">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+        </section>
+    )
+}
